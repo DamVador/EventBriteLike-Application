7526bbe21409d7bcd369acdeb0ad7bcd443f2df6
Damien <damienbanville@hotmail.fr>
2020-08-11 16:34:19 +0200

    profile added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,5 +9,14 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(title: params[:title], content: params[:content], user_id: current_user.id) # avec xxx qui sont les données obtenues à partir du formulaire
+
+    if @event.save # essaie de sauvegarder en base @gossip
+      render "/"# si ça marche, il redirige vers la page d'index du site
+    else
+      render :new
+    end
   end
+
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
-#before_action :authenticate_user
+before_action :authenticate_user!
 
   def show
     unless current_user
       flash[:danger] = "Please log in."
       redirect_to new_session_path
     else
-      if current_user.id == params[:id]
-        flash[:danger] = "Ce n'est pas la page de votre profile"
-        redirect_to event_path
-      end
-      @user = User.find(params[:id]).user
+      #if current_user.id != params[:id]
+        #flash[:danger] = "Ce n'est pas la page de votre profile"
+      #  redirect_to event_path
+    #  end
+      @user = User.find(params[:id])
     end
   end
+
 end
