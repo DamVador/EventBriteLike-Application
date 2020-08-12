8251ce28dacf1be6dbfe98042fa0cbe4afdc9222
Damien <damienbanville@hotmail.fr>
2020-08-12 18:59:35 +0200

    added conditions on the user who wanna join an event, and generated routes for attendances but also controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'attendances/new'
+  get 'attendances/create'
+  get 'attendances/show'
+  get 'attendances/destroy'
   devise_for :users
   resources :users
   resources :events
+  resources :attendances
   root :to => "events#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
